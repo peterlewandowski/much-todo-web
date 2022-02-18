aa95a9e0b69a8e76b3d012670c73b1b27c24fb42
Peter Lewandowski <mapiotr2010@gmail.com>
2022-02-18 14:58:03 -0500

    using .map instead of List component

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -2,12 +2,17 @@ import { Input } from 'antd';
 import { useState } from 'react'
 
 export default function NewTask() {
-    const [task, setTask] = useState('');
+    const [newTask, setTask] = useState('');
 
+    const taskObject = {
+        task: 'Buy Milk',
+        done: false
+    }
 
   return (
     <>
-      <Input placeholder='Enter task name' />
+      <Input placeholder='Enter task name' 
+      onChange={event => console.log(event.target.value)} />
     </>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,30 +1,45 @@
-import { useState, useEffect } from 'react';
-import { List } from 'antd';
-import Task from './Task'
+import { useState, useEffect } from "react";
+import { List } from "antd";
+import Task from "./Task";
 
-const fakeTasks = [
-    { id: 1, task: 'Buy Beer', done: false },
-    { id: 2, task: 'Buy Trulys', done: true },
-    { id: 3, task: 'Buy Milk', done: false },
-    { id: 4, task: 'Buy Wine', done: true },
-    { id: 5, task: 'Buy Paper Towels', done: false },
-]
+// const fakeTasks = [
+//     { id: 1, task: 'Buy Beer', done: false },
+//     { id: 2, task: 'Buy Trulys', done: true },
+//     { id: 3, task: 'Buy Milk', done: false },
+//     { id: 4, task: 'Buy Wine', done: true },
+//     { id: 5, task: 'Buy Paper Towels', done: false },
+// ]
 
 export default function TaskList() {
-    const [tasks, setTasks] = useState(fakeTasks)
-    useEffect(() => {
-        fetch('https://much-todo-pl.uc.r.appspot.com/tasks')
-            .then(response => response.json())
-            .then(data => setTasks(data))
-            .catch(alert)
-    }, [])
+  const [tasks, setTasks] = useState();
+  useEffect(() => {
+    // Get the data from the API
+    fetch("https://much-todo-pl.uc.r.appspot.com/tasks")
+      .then((response) => response.json())
+      .then((data) => setTasks(data))
+      .catch(alert);
+  }, []);
 
-    return (
-        <List
-            bordered
-            dataSource={tasks}
-            renderItem={item => <Task item={item} />} // importing {item} from Task
-        />
-    )
-    
+  // tasks has all the data
+
+  // we need to pass this TASKS => data to the list component
+  // return (
+  //     <List
+  //         bordered
+  //         dataSource={tasks}
+  //         renderItem={item => <Task item={item} />} // importing {item} from Task
+  //     />
+  // )
+
+  return (
+    <>
+      <ul>
+        {tasks && tasks.map(singleTask => {
+            return (
+                <li>{singleTask.id} ------> {singleTask.task}</li>
+            )
+        })}
+      </ul>
+    </>
+  )
 }
