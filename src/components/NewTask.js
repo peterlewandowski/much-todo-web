186c6a327e1cffd4459be7bb1fade5e90321f56a
Peter Lewandowski <mapiotr2010@gmail.com>
2022-02-20 22:44:18 -0500

    added object destructuring as a comment

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,42 +1,44 @@
-import { Input } from 'antd';
-import { useState } from 'react'
+import { Input } from "antd";
+import { useState } from "react";
 
 export default function NewTask() {
+  const [newTask, setNewTask] = useState("");
 
-    const [newTask, setNewTask] = useState('');
+  const taskObject = {
+    task: newTask,
+  };
 
-    const taskObject = {
-        task: newTask,
-    }
+  const handleButtonSubmit = () => {
+    console.log("sending to API");
 
-    const handleButtonSubmit = () => {
-        console.log('sending to API')
+    fetch("https://much-todo-pl.uc.r.appspot.com/tasks", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify(taskObject),
+    })
+      .then()
+      .then()
+      .catch((err) => alert(err));
+  };
 
-        fetch('https://much-todo-pl.uc.r.appspot.com/tasks', {
-            method: 'POST',
-            headers: {
-                'content-Type': 'application/json',
-        },
-        body: JSON.stringify(taskObject),
-        })
-        .then()
-        .then()
-        .catch(err => alert(err))
-    }
+  const handleInputText = (event) => {
+    setNewTask(event.target.value);
+  };
 
-    const handleInputText = (event) => {
-        setNewTask(event.target.value)
-    }
+  // const handleInputText = ({target}) => setNewTask(target.value) // the above lines can be written with object destructuring
 
-    console.log('newTask state here ->>', newTask)
+  console.log("newTask state here -->>", newTask);
 
   return (
     <>
-    <h2>Add new task</h2>
-      <Input 
-      placeholder='Enter task name' 
-      onChange={event => handleInputText(event) } />
-      
+      <h2>Add new task</h2>
+      <Input
+        placeholder="Enter task name"
+        onChange={(event) => handleInputText(event)}
+      />
+
       <button onClick={handleButtonSubmit}>Send new task to API</button>
     </>
   );
